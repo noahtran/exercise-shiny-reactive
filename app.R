b7e8a24897c9ebb8852e9038224e9a1603f58216
Andrey Butenko <ghostmancer@gmail.com>
2019-05-22 10:19:17 -0700

    Add solution

--- app.R
+++ app.R
@@ -6,41 +6,74 @@ library(ggplot2)
 # Data from the Social Security Administration
 data <- read.csv("wa_unhealthy_air.csv", stringsAsFactors = FALSE)
 
-
-# UI
 ui <- fluidPage(
+  theme = shinytheme("superhero"),
   titlePanel("WA Unhealthy Air"),
   
-  # A few paragraphs to give context
   p("We are examining the percentage of days with unsafe PM2.5 percentages in different counties of Washington State."),
   p("PM2.5 refers to particles in the air that are tiny and dangerous to inhale."),
+  p("We want to see how this has changed over time."),
     
   sidebarLayout(
     sidebarPanel(
-      # Put a widget here for the user to interact with!
-      # You could filter by year, by county, etc.
+      selectInput("chosenCounties",
+                  label = "Counties to view:",
+                  choices = unique(data$CountyName),
+                  selected = unique(data$CountyName),
+                  multiple = TRUE),
+      
+      sliderInput("chosenYears",
+                  "Year",
+                  min = min(data$ReportYear),
+                  max = max(data$ReportYear),
+                  value = c(min(data$ReportYear), max(data$ReportYear)),
+                  sep = "",
+                  step = 1)
     ),
-    
     mainPanel(
       tabsetPanel(
-        tabPanel("Plot", plotOutput("airQualityPlot")),
-        tabPanel("Table", tableOutput("airQualityText"))
+        tabPanel("Plot over time", plotOutput("airQualityPlot")),
+
+        tabPanel("Text summary", textOutput("airQualityText")),
+
+        tabPanel("Table", tableOutput("airQualityTable"))
       )
     )
   )
 )
 
-# Server
+# Define server logic required to draw a histogram
 server <- function(input, output) {
-  # A plot of air quality
-  # This can be over time, by county, etc.
-  output$airQualityPlot <- renderPlot({
-    
+  chosen_data <- reactive({
+    data %>%
+      filter(CountyName %in% input$chosenCounties,
+             ReportYear >= input$chosenYears[1],
+             ReportYear <= input$chosenYears[2])
   })
   
-  # A table of the filtered data
-  output$airQualityTable <- renderTable({
+  output$airQualityPlot <- renderPlot({
+    chosen_data() %>% 
+      ggplot() +
+      geom_line(aes(x = ReportYear, y = Value, color = CountyName)) +
+      labs(title = "Washington state air quality over time",
+           subtitle = "Percent of days with PM2.5 levels over the National Ambient Air Quality Standard (NAAQS)",
+           x = "Year",
+           y = "Percent of unsafe days")
+  })
+  
+  output$airQualityText <- renderText({
+    lowest <- chosen_data() %>% 
+      group_by(CountyName) %>% 
+      summarize(mean_percent = mean(Value)) %>% 
+      filter(mean_percent == min(mean_percent))
     
+    paste0("For chosen years and chosen counties, the average percent of days with PM2.5 levels over the air quality standard was ",
+           round(mean(chosen_data()$Value), 2), "%. ",
+           "The county with the lowest percentage of unsafe days was ", lowest$CountyName, " at ", round(lowest$mean_percent, 2), "%!")
+  })
+  
+  output$airQualityTable <- renderTable({
+    chosen_data()
   })
 }
 
